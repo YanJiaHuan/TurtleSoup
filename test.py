680aee7c150f72e39c89c84c2ca06b3efca4f5b5
William yan <1337842463@qq.com>
2023-07-28 17:29:49 +0800

    initial

--- test.py
+++ test.py
@@ -1,19 +1,51 @@
+import openai
 import gradio as gr
-from transformers import pipeline
-def greet(name):
-    return "Hello " + name + "!"
+import random
+import time
 
-pipe = pipeline('text-classification',model='distilbert-base-uncased-finetuned-sst-2-english', tokenizer='distilbert-base-uncased-finetuned-sst-2-english')
-def clf(text):
-    result = pipe(text)
-    label = result[0]['label']
-    score = result[0]['score']
-    print(label, score)
-    res = {label: score, 'Postive' if label == 'Negative' else 'Negative': 1 - score}
-    return res
-test_text = "是的，他有过出海的经历。"
-print(clf(test_text))
+model_name = "gpt-3.5-turbo"
+openai.api_key = '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'
 
-demo = gr.Chatbot(fn=greet, inputs="text", outputs="text")
-gr.close_all()
+def GPT_generation(prompt):
+    '''
+    openai.error.RateLimitError: Rate limit reached for default-gpt-3.5-turbo
+    in organization org-GFmlumrCZBB2Y40fVv7f8qgp on requests per min. Limit: 3 / min.
+    Please try again in 20s. Contact us through our help center at help.openai.com if you continue to have issues.
+    Please add a payment method to your account to increase your rate limit.
+    Visit https://platform.openai.com/account/billing to add a payment method.
+    '''
+    while True:
+        try:
+            response = openai.ChatCompletion.create(
+                model=model_name,
+                messages=[{"role": "user", "content": prompt}],
+                n = 1,
+                stream = False,
+                temperature=0.0,
+                max_tokens=600,
+                top_p = 1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0,
+            )
+            return response['choices'][0]['message']['content']
+
+        except openai.error.RateLimitError as e:
+            time.sleep(20)
+            return "Rate limit reached. Please try again in 20s."
+        except Exception as e:
+            return "Unexpected error. Please try again."
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+    def respond(message, chat_history):
+        bot_message = GPT_generation(message)
+        chat_history.append((message, bot_message))
+        time.sleep(2)
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
 demo.launch(share=True)
